e67db7a737efc1225af567b0dabdc885f8d25a5f
crisluda <crisluda2244@gmail.com>
2020-01-28 15:46:19 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+var express = require("express");
+app = express();
+var PORT = 3000
+var bodyParser = require("body-parser");
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+var ejs = require("ejs");
+app.set("view engine", "ejs");
+var path = require('path');
+app.use(express.json())
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/blog", {
+    useNewUrlParser: true
+});
+var blogSchema = mongoose.model("blogSchame", {
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// blogSchema.create(
+//   {
+//     title: "OPPO A9",
+//     image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-a9-2020-.jpg",
+//     body:
+//       "The A9 is a budget smartphone from smartphone maker Oppo. It has a big 6.5-inch fullHD+ display at the front with a waterdrop notch. ... Oppo has opted for the MediaTek Helio P70 Soc to power the A9. It is available in one variant only with 4GB of RAM and 128GB of storage."
+//   },
+//   function(err, data) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(blogSchema);
+//     }
+//   }
+// );
+app.get("/", function (req, res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function (req, res) {
+
+    blogSchema.find({}, function (err, blogs) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("index", {
+                blogs: blogs
+            });
+        }
+    })
+});
+
+app.get("/blogs/new", function (req, res) {
+    res.render("new")
+})
+app.post("/blogs", function (req, res) {
+    blogSchema.create(req.body.blog, function (err, newBlogs) {
+        if (err) {
+            res.render("new")
+        } else {
+            res.redirect("/blogs")
+        }
+    })
+})
+app.get("/blogs/:id", function (req, res) {
+    blogSchema.findById(req.params.id, function (err, blog) {
+        if (err) {
+            res.redirect("/blogs")
+        } else {
+            res.render("show", {
+                blog: blog
+            })
+        }
+    })
+})
+app.get("/*", function (req, res) {
+    res.send("Err page not find");
+});
+
+app.listen(process.env.PORT || PORT, function () {
+    console.log("server is runing " + PORT);
+});
